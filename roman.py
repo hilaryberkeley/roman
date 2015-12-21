7ccf89801df044b89e6c6e0f7b1946181d6666b1
kunstschmiede <hsburgess@gmail.com>
2015-12-21 15:51:03 -0800

    this sucks

--- roman.py
+++ roman.py
@@ -1,4 +1,35 @@
-# This is a program which can be used to convert integers to roman numerals, and reverse.
+# This is a program which can be used to convert arabic to roman numerals, and reverse.
 
-def function():
-	pass
+import sys
+import math
+
+
+NumMap = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5, 'VI':6, 'VII':7, 'VIII':8, 'IX':9, 'X':10,
+'L':50, 'C':100, 'D':500, 'M':1000
+}
+
+arNum = int(raw_input('Please enter a zero to four digit number: ')[:4])
+lengarNum = len(str(arNum))
+# print lengarNum
+
+
+if lengarNum == 1:
+	def find_key(NumMap, arNum):
+		for k, v in NumMap.items():
+			if v == arNum:
+				yield k
+				print next(find_key(NumMap, 'arNum'), None)
+
+	# if arNum in NumMap:
+	# 	print key
+	# 	print 'Your roman numeral is: %s', % NumMap[arNum]
+
+else:
+	print "whatever"
+
+
+
+# elif len(arNum) = 2:
+
+# elif len(arNum) = 3:
+# elif len(arNum) = 4:
