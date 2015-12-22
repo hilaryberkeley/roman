e8e2ad524cc6192c9cd427eb967f076615cd6948
kunstschmiede <hsburgess@gmail.com>
2015-12-21 22:15:42 -0800

    almost finished

--- roman.py
+++ roman.py
@@ -1,35 +1,49 @@
 # This is a program which can be used to convert arabic to roman numerals, and reverse.
+# Due to the "bar" symbol on Roman numerals 4,000 and up, I limited this to 3,999.
 
-import sys
-import math
+# Roman Numeral Array
+RomArray = ['MMM', 'MM', 'M', 'CM', 'DCCC', 'DCC', 'DC', 'D', 'CD', 'CCC', 'CC', 'C', 
+			'XC', 'LXXX', 'LXX', 'LX', 'L', 'XL', 'XXX', 'XX', 'X', 'IX', 'VIII', 
+			'VII', 'VI', 'V', 'IV', 'III', 'II', 'I']
+
+# Arabic Numeral Array
+AraArray = [3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60,
+			50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+def arabic_to_roman(ar_num):
+	numeral = ''
+	for i, v in enumerate(AraArray):
+		into = ar_num / v
+		if into == 1:
+			numeral += RomArray[i]
+			remainder = ar_num - v
+			ar_num = remainder	
+	return numeral
+
+# Get a four digit Arabic Numeral from a user, convert it to a Roman Numeral
+u_arabic = raw_input('Please enter a number between 1 and 3999: ')
+u_arabic = int(u_arabic)
+
+print "Your Roman numeral is: " + arabic_to_roman(u_arabic)
+
+print '-' * 20
+
+# Get a Roman Numeral from a user, convert it to an Arabic Numeral
+u_roman = raw_input('Please enter a Roman Numeral between 1 and 3999: ')
+
+def roman_to_arabic(ro_num):
+	numeral = int()
+	for i, v in enumerate(RomArray):
+		if v in ro_num:
+			numeral += AraArray[i]
+			
+			print numeral
+	return numeral
+
+print "Your Arabic Numeral is: " + str(roman_to_arabic(u_roman))
 
 
-NumMap = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5, 'VI':6, 'VII':7, 'VIII':8, 'IX':9, 'X':10,
-'L':50, 'C':100, 'D':500, 'M':1000
-}
-
-arNum = int(raw_input('Please enter a zero to four digit number: ')[:4])
-lengarNum = len(str(arNum))
-# print lengarNum
-
-
-if lengarNum == 1:
-	def find_key(NumMap, arNum):
-		for k, v in NumMap.items():
-			if v == arNum:
-				yield k
-				print next(find_key(NumMap, 'arNum'), None)
-
-	# if arNum in NumMap:
-	# 	print key
-	# 	print 'Your roman numeral is: %s', % NumMap[arNum]
-
-else:
-	print "whatever"
 
 
 
-# elif len(arNum) = 2:
 
-# elif len(arNum) = 3:
-# elif len(arNum) = 4:
